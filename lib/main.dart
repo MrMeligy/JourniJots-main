3291e3581f16269775dc5cce3ae2a532b39c6303
MrMeligy <youssefelzyat72@gmail.com>
2025-03-16 22:04:48 +0200

    fix text bug on release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:journijots/core/routes/app_routing.dart';
 import 'package:journijots/journijots_app.dart';
 
-void main() {
+void main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(
     JournijotsApp(
       appRouting: AppRouting(),
